c7a3d7e7adc7030eb66ea4815f14c50cd1ad5b5f
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-18 00:47:52 +0800

    aaa

--- src/app/_components/NavigationProfile.tsx
+++ src/app/_components/NavigationProfile.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { LuCoffee } from "react-icons/lu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { IoIosLogOut } from "react-icons/io";
@@ -10,22 +11,31 @@ import {
 } from "@/components/ui/select";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { cookies } from "next/headers";
 import { data } from "../dashboard/page";
 import { Logout } from "./logout";
 
 export async function NavigationProfile() {
-  let dat = null;
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashbordInfo`, {
-      credentials: "include",
-      headers: { Cookie: (await cookies()).toString() },
-    });
-    dat = await res.json();
-  } catch (e) {
-    console.error(e, "aldaa");
-  }
+  const [dat, setData] = useState<data>();
+
+  useEffect(() => {
+    const fetData = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/dashbordInfo`,
+          {
+            credentials: "include",
+          }
+        );
+        const data = await res.json();
+        setData(dat);
+      } catch (e) {
+        console.error(e, "aldaa");
+      }
+    };
+    fetData();
+  }, []);
   return (
     dat && (
       <div className="bg-slate-100 w-screen ">
